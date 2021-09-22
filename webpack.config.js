51b8c75a3a242400d963700cb4477bf21ef22544
Alex Wardill <alexander@wardill.net>
2021-09-22 23:39:24 +0100

    output:clean, added variables to Html Plugin object

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ const path = require('path');
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack Output",
+        hash: true,
+        title: 'To Do List',
+        header: 'Another To List App',
+        metaDesc: 'Webpack Example Description',
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
     }),
   ],
   mode: 'development'
